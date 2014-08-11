3f83080098cebd3c9ce1c0641aaf0319ecd712c0
Jason Favrod <jason@orbiter.starbase.lan>
2014-08-10 22:47:39 -1000

    Added Shopping Cart
    Just getting started with the shopping cart module.

---
+++ resources/php/ShoppingCart.php
@@ -0,0 +1,41 @@
+<?php
+/**
+ * A Generic Virtual Shopping Cart
+ *
+ */
+class ShoppingCart
+{
+    /**
+     * Id of $this Shopping Cart.
+     *
+     * @var string
+     */
+    private $id;
+
+    /**
+     * A container for CartItems.
+     *
+     * @var array
+     */
+    private $items = [];
+
+    /**
+     * todo: A place to store the cart for later.
+     */ 
+
+    public function __construct($id) {
+        $this->id = $id;
+    }
+
+    public function add_item(CartItem $item) {
+        array_push($this->items, $item);
+    }
+
+    public function __toString() {
+      $str = 'id: '.$this->id.",\n";
+      if (!empty($this->items)) {
+          return;
+      }
+      return $str;
+    }
+}
