92a34b4c628ea699c6292e0b8e0d9df64affe377
not--p <lakona808@gmail.com>
2014-12-16 21:33:06 -1000

    Refactored and Renamed get_page_by_uri in Core Controler
    Changed name to include_page_by_uri, which is acturally what it does.
    
    index.php is now calling by new function name.

--- controller.php
+++ controller.php
@@ -6,30 +6,22 @@ require_once('config.php');
 use Registry;
 
 /**
- * Grab the server request URI, trim off the leading slash, look inside
- * a specified for a php file with the same name as the URI. If there's
- * such a file, return an include statement with the file specified as
- * its argument.
+ * Grabs the request URI, parses it, then includes on the page any
+ * requested page that is in the PAGES_DIR.
  *
- * @todo use filter_input() instead of referring to $_SERVER (actually
- * client) data directy.
- *
- *     NOTE: Includes a 404.php page by default.
- *
- * @param string $pages_dir 'pages' by default, otherwise the directory
- * from where pages are included from.
- * @return include A php file is included in the document where called.
+ * @return void
  */
-function get_page_from_uri($pages_dir='pages') {
-    $URI = ltrim($_SERVER['REQUEST_URI'], '/');
 
-    if ($URI == "" && $default_page = Registry::get('Nav')->get_default_page()){
-        include($pages_dir."/$default_page");
+function include_page_from_uri() {
+    $URI = ltrim(filter_input(INPUT_SERVER, 'REQUEST_URI', FILTER_SANITIZE_URL), '/');
+
+    if ($URI === '' && DEFAULT_PAGE){
+        include(PAGES_DIR.'/'.DEFAULT_PAGE);
     }
-    else if (in_array("$URI.php", scandir("$pages_dir"))) {
-        include("$pages_dir/$URI.php");
+    else if (in_array("$URI.php", scandir(PAGES_DIR))) {
+        include(PAGES_DIR."/$URI.php");
     }
     else {
-        include("$pages_dir/404.php");
+        include(PAGES_DIR.'/404.php');
     }
 }
