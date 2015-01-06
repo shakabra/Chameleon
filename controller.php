3f95564961e929c4739d47a32a4dccf75c741f52
not--p <lakona808@gmail.com>
2015-01-06 13:32:47 -1000

    Chameleon Specific Index Content Now Generated in Controller
    Now users can add their unique content to the site and easily avoid
    collisions with the necessary components of Chameleon.

--- controller.php
+++ controller.php
@@ -26,3 +26,41 @@ function include_page_from_uri() {
         include(PAGES_DIR.'/404.php');
     }
 }
+
+
+/**
+ * Prints the required HTML header data (based on config).
+ */
+
+function print_site_header()
+{
+    print
+  '<meta charset="utf-8">
+  <meta name="viewport" content="width=device-width, initial-scale=1">
+  <title>'.SITE_TITLE.'</title>
+
+  <!--<script src=""></script>-->
+
+  <link rel="stylesheet" href="'.BOOTSTRAP_DIR.'/css/bootstrap.min.css">
+  <link rel="stylesheet" href="'.CSS_DIR.'/layout.css">
+  <link rel="stylesheet" href="'.CSS_DIR.'/formatting.css">
+
+  <!-- HTML5 Shim and Respond.js IE8 support of HTML5 elements and media queries -->
+  <!-- WARNING: Respond.js doesn\'t work if you view the page via file:// -->
+  <!--[if lt IE 9]>
+    <script src="https://oss.maxcdn.com/libs/html5shiv/3.7.0/html5shiv.js"></script>
+    <script src="https://oss.maxcdn.com/libs/respond.js/1.4.2/respond.min.js"></script>
+  <![endif]-->';
+}
+
+
+/**
+ * Prints the script tags required for Chameleon/Bootstrap to function.
+ */
+
+function get_required_scripts()
+{
+    print
+  '<script src="'.JS_DIR.'/jquery-1.11.2.min.js"></script>
+  <script src="'.BOOTSTRAP_DIR.'/js/bootstrap.min.js"></script>';
+}
--- index.php
+++ index.php
@@ -6,29 +6,14 @@ require_once('controller.php');
 
 <!DOCTYPE html>
 <html lang="en">
-<head>
-  <meta charset="utf-8">
-  <meta name="viewport" content="width=device-width, initial-scale=1">
-  <title><?php echo SITE_TITLE; ?></title>
 
-  <!--<script src=""></script>-->
+  <head>
+    <?php print_site_header(); ?>
+  </head>
 
-  <link rel="stylesheet" href="<?php echo BOOTSTRAP_DIR; ?>/css/bootstrap.min.css">
-  <link rel="stylesheet" href="<?php echo CSS_DIR; ?>/layout.css">
-  <link rel="stylesheet" href="<?php echo CSS_DIR; ?>/formatting.css">
+  <body>
+    <?php include_page_from_uri(); ?>
+    <?php get_required_scripts(); ?>
+  </body>
 
-  <!-- HTML5 Shim and Respond.js IE8 support of HTML5 elements and media queries -->
-  <!-- WARNING: Respond.js doesn't work if you view the page via file:// -->
-  <!--[if lt IE 9]>
-    <script src="https://oss.maxcdn.com/libs/html5shiv/3.7.0/html5shiv.js"></script>
-    <script src="https://oss.maxcdn.com/libs/respond.js/1.4.2/respond.min.js"></script>
-  <![endif]-->
-</head>
-<body>
-
-  <?php include_page_from_uri(); ?>
-
-  <script src="<?php echo JS_DIR.'/jquery-1.11.2.min.js'; ?>"></script>
-  <script src="<?php echo BOOTSTRAP_DIR; ?>/js/bootstrap.min.js"></script>
-</body>
 </html>
