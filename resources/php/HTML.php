0eea298ba3f9155bb5a7eaf08cab2a142fd1ec41
not--p <jason@lakonacomputers.com>
2015-03-24 10:55:21 -1000

    Added HTML::table HTML::simpleElement
    Tags such as h* and p use the simepleElement method.

--- resources/php/HTML.php
+++ resources/php/HTML.php
@@ -4,37 +4,80 @@ class HTML
 {
     private static $element_count = [];
 
-    public static function p ($spec=[], $type=__method__)
+    public static function simpleElement (&$spec, &$type)
     {
-        $text = "";
-        $type = ltrim($type, "HTML::");
+        $text = '';
+        $type = ltrim($type, 'HTML::');
+
+        if (is_array($spec) && array_key_exists('id', $spec))
+            $id = $spec['id'];
+        else
+            $id = self::getID($type);
 
         if      (is_array($spec))    $text = $spec["text"];
         else if (is_string($spec))   $text = $spec;
 
-        return "<" . $type . " id=\"" . self::getID($type) . "\">" . $text .
-            "</" . $type . ">";
+        return "<$type id=\"$id\">$text</$type>\n";
     }
-
 
     public static function h1 ($spec=[], $type=__method__) {
-        return self::p($spec, $type);
+        return self::simpleElement($spec, $type);
     }
-
     public static function h2 ($spec=[], $type=__method__) {
-        return self::p($spec, $type);
+        return self::simpleElement($spec, $type);
     }
-
     public static function h3 ($spec=[], $type=__method__) {
-        return self::p($spec, $type);
+        return self::simpleElement($spec, $type);
     }
-
     public static function h4 ($spec=[], $type=__method__) {
-        return self::p($spec, $type);
+        return self::simpleElement($spec, $type);
+    }
+    public static function h5 ($spec=[], $type=__method__) {
+        return self::simpleElement($spec, $type);
+    }
+    public static function p ($spec=[], $type=__method__) {
+        return self::simpleElement($spec, $type);
     }
 
-    public static function h5 ($spec=[], $type=__method__) {
-        return self::p($spec, $type);
+
+    /**
+     * table
+     *
+     * An HTML table generated base upon $spec parameter.
+     *
+     * @param array $spec HTML table specification.
+     * @return String The generated HTML table.
+     */
+
+    public static function table($spec=[])
+    {
+        if (array_key_exists('id', $spec)) $id = $spec['id'];
+        else                               $id = self::getID('table');
+
+        $markup = "<table id=\"$id\">\n";
+
+        if (array_key_exists('cols', $spec))
+        {
+            $markup .= "\t<tr>";
+
+            foreach ($spec['cols'] as $attribute) {
+                $markup .= "<th>$attribute</th>";
+            }
+
+            $markup .= "</tr>\n";
+        }
+
+        foreach ($spec['rows'] as $row) {
+            $markup .= "\t<tr>";
+
+          foreach ($row as $data) {
+            $markup .= "<td>$data</td>";
+          }
+
+          $markup .= "</tr>\n";
+        }
+
+        return $markup .= "</table>\n";
     }
 
 
