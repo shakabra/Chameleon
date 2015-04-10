8f8203e3ef235f44358881bac3881f9536fc9c28
not--p <jason@lakonacomputers.com>
2015-04-09 17:18:21 -1000

    Removed Authentication Module; PSR Stlye Edits on Registery

--- resources/php/Registry.php
+++ resources/php/Registry.php
@@ -9,38 +9,43 @@ class Registry
 {
     private static $_store = array();
 
-    public static function add($object, $name=null) {
-        !is_null($name)?: $name = get_class($object);
+    public static function add($object, $name=null)
+    {
+        if (is_null($name)) $name = get_class($object);
 
-        if (isset(self::$_store[$name])) {
+        if (isset(self::$_store[$name]))
             throw new Exception('Already an object: '. $name .' in the Registry');
-        }
 
         self::$_store[$name] = $object; 
     }
 
-    public static function get($name) {
-        if (!array_key_exists($name, self::$_store)) {
+    public static function get($name)
+    {
+        if (!array_key_exists($name, self::$_store))
             throw new Exception('Item: '. $name .' not in the Registry');
-        }
+
         return self::$_store[$name];
     }
 
-    public function __toString() {
+    public function __toString()
+    {
         $_string = '';
-        foreach(self::$_store as $name => $object) {
+        foreach (self::$_store as $name => $object)
             $_string .= $name. ': ' .get_class($object) . "\n";
-        }
+
         return $_string;
     }
 
-    public function toHtml() {
+    public function toHtml()
+    {
         $_html = "<table class=\"php-obj-table\">\n";
         $_html .= "\t<tr><th>Name</th><th>Type</th></tr>\n";
-        foreach(self::$_store as $name => $object) {
+
+        foreach(self::$_store as $name => $object)
             $_html .= "\t<tr><td>$name</td><td>".get_class($object)."</td></tr>\n";
-        }
+
         $_html .= "</table>\n";
         return $_html;
     }
 }
+
