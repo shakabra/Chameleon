b7c7410529dcf60fddb0e538505d987e01c7c675
not--p <jason@lakonacomputers.com>
2015-05-10 09:23:03 -1000

    Continuing Work on Setup Script
    * Checking for admin or root user.
    * Using exit_code with sys.exit to provide informative exit condition
    codes.
    * Added function to set the needed Apache configuration files/locations.

--- setup.py
+++ setup.py
@@ -1,4 +1,6 @@
 #!/usr/bin/env python
+import os
+import sys
 import platform
 
 
@@ -10,15 +12,25 @@ virtual host and rewrite module.
 
 return 0 : success
 return 1 : general fail
+return 5 : insufficent priviledges
 """
 def main():
     exit_code = 0
     config = {}
 
-    config['platform']     = platform.system().lower()
-    config['proj_name']    = ask_name('project');
-    config['default_view'] = ask_name('view');
-    print (config)
+    if (admin_or_root()):
+        config['platform']     = platform.system().lower()
+        config['proj_name']    = ask_name('project')
+        config['default_view'] = ask_name('view')
+
+        if (config['platform'] == 'linux'):
+            config['dist'] = platform.linux_distribution()[0].lower()
+
+        set_apache_locations(config)
+        print (config)
+    else:
+        print('Insufficent Priviledges\nPlease run as root or admin user.')
+        exit_code = 5
     
     # Create a Virtual Host for Project.
     
@@ -28,7 +40,23 @@ def main():
 
     # Create Config for the Project.
     
-    return exit_code
+    sys.exit(exit_code)
+
+
+"""
+admin_or_root
+
+Checks for adminstrator or root priviledges.
+
+return True  : Sufficent priviledges.
+return False : Insufficent priviledges.
+"""
+def admin_or_root():
+    auth = False
+    if (os.geteuid() == 0):
+        auth = True
+
+    return auth
 
 
 """
@@ -56,6 +84,19 @@ def ask_name(purpose):
             print('Invalid Project Name')
 
     return name
+
+
+"""
+set_apache_locations
+
+Finds the needed Apache configuration locations/files, and stores them
+in the configuration dict.
+"""
+def set_apache_locations(config):
+    if (config['platform'] == 'linux'):
+        if (config['dist'] == 'ubuntu' or config['dist'] == 'debian'):
+            config['apache_conf'] = os.path.abspath('/etc/apache2/apache2.conf')
+            config['vhost_dir'] = os.path.abspath('/etc/apache2/sites-available')
 
 
 """
